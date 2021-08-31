7e473e261949d483a45bd2dfdcfe87e21924ffb4
Raul <Raul@DESKTOP-06T2O8P>
2021-08-31 23:09:35 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.run();
+            car.stop();
+        }
+    }
+    abstract class Vehiculable
+    {
+        public void run()
+        {
+            Console.WriteLine("arranco desde la clase abstacta");
+        }
+    }
+
+    interface IVehiculable
+    {
+        //public void run();
+        public void stop();
+    }
+    class Car : Vehiculable, IVehiculable
+    {
+        public void stop()
+        {
+            Console.WriteLine("paro");
+        }
+        new public void run()
+        {
+            Console.WriteLine("arranco desde coche");
+        }
+    }
+}
